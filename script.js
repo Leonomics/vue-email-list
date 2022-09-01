cc86984e4e934c8c4c43495f1a2225e8bc8a6d7d
Leonomics <leocv97@gmail.com>
2022-09-01 15:25:20 +0200

    print objects on document

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 const app = new Vue({
     el:'#app',
     data:{
-        
+        res:'',
     },
     mounted(){
         axios
             .get('https://flynn.boolean.careers/exercises/api/random/mail')
             .then((res) => {
-                console.log(res);
+                this.res = res
                 
             })
-    }
+    },
+    
 });
-
