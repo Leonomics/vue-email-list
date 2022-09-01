4371cfddebafe6ef16f2a65537615d71fef36a9b
Leonomics <leocv97@gmail.com>
2022-09-01 15:12:07 +0200

    add axios

---
+++ script.js
@@ -0,0 +1,15 @@
+const app = new Vue({
+    el:'#app',
+    data:{
+        
+    },
+    mounted(){
+        axios
+            .get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then((res) => {
+                console.log(res);
+                
+            })
+    }
+});
+
